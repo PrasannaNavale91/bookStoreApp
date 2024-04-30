788d9d64ca9086f03a91532da1a2863fd660ce6d
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2024-04-30 13:38:41 +0530

    Frontend done!!

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Home from './home/Home';
+import { Route, Routes } from "react-router-dom";
+import Courses from './courses/Courses';
+import Signup from './components/Signup';
+import About from './components/About';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <>
+      <div className='dark:bg-slate-900 dark:text-white'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/course' element={<Courses />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const About = () => {
+  return (
+    <>
+      <Navbar />
+        <div className='mx-w-screen-2xl container mx-auto md:px-20 px-4 dark:bg-slate-800 dark:text-white'>
+            
+        </div>
+      <Footer />
+    </>
+  )
+}
+
+export default About;
